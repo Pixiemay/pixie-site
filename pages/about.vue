af7edfd822c8883cb93c14393a7a0965809b3c73
Pixiemay <kondakov20007.andrey@gmail.com>
2024-06-01 13:06:41 +0700

    аура F

--- pages/about.vue
+++ pages/about.vue
@@ -35,11 +35,11 @@
           <h1 class="bg-gradient-to-r from-indigo-500 to-blue-500 bg-clip-text text-transparent text-3xl font-bold w-32 mx-auto mt-4">MineStar</h1>
           <h2 class="text-gray-400 font-semibold text-base text-center md:text-lg mt-2">Мой первый сервер майнкрафт, на котором я получил больше всего опыта.  Май 2021г. - Март 2024г.</h2>
         </div>
-        <a class="mt-8 xl:mt-16 h-76 xl:h-5/6 lg:h-56 bg-zinc-800 hover:bg-zinc-700/60 rounded-3xl px-8 py-8 hover:scale-105 duration-500 ease-in-out w-64 xl:w-72 mx-auto" href="https://discord.gg/JxqujFfRNE">
+        <div class="mt-8 xl:mt-16 h-76 xl:h-5/6 lg:h-56 bg-zinc-800 hover:bg-zinc-700/60 rounded-3xl px-8 py-8 hover:scale-105 duration-500 ease-in-out w-64 xl:w-72 mx-auto" href="https://discord.gg/JxqujFfRNE">
           <img src="../assets/img/aura.png" class="size-48 xl:size-52 rounded-3xl mx-auto">
           <h1 class="bg-gradient-to-r from-fuchsia-600 to-purple-600 bg-clip-text text-transparent text-3xl font-bold w-20 mx-auto mt-4">Aura</h1>
           <h2 class="text-gray-400 font-semibold text-base text-center md:text-lg mt-2">Сервер на котором я являлся администратором совместно с An1by. Март 2024г. - Июнь 2024г.</h2>
-        </a>
+        </div>
       </div>
     </div>
   </div>
