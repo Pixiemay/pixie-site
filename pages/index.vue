73693673784a811026679650d99a3681ddac4073
Pixiemay <kondakov20007.andrey@gmail.com>
2024-06-01 16:22:19 +0700

    Мобильная адаптация вроде

--- pages/index.vue
+++ pages/index.vue
@@ -4,21 +4,26 @@ import AboutBut from "~/components/AboutBut.vue";
 </script>
 
 <template>
-  <div class="w-full bg-zinc-900 lg:h-screen xl:h-screen xl:fixed">
-    <div class="pt-16 xl:mt-40 xl:mb-32 lg:mb-16 mx-20 md:mx-44 lg:mx-auto w-1/2 h-1/6 xl:h-2/6 animate-in fade-in-5 duration-1000">
-      <img src="../assets/img/komar.jpg" class="rounded-full size-28 md:size-36 lg:size-32 xl:size-36 mx-12 lg:mx-auto">
-      <div class="w-screen md:flex md:flex-row xl:mx-52">
-        <h1 class="text-white text-2xl md:text-4xl lg:text-4xl font-semibold tracking-normal mt-8">Привет! Меня зовут </h1>
-        <span class="bg-gradient-to-r from-indigo-400 to-indigo-600 bg-clip-text text-transparent text-4xl lg:text-4xl font-bold mt-2 md:mt-8 ml-8 md:ml-2 type-mynames"></span>
+  <div class="min-h-screen bg-zinc-900 xl:h-full">
+    <div class="pt-24 xl:pt-48 xl:mx-auto xl:mb-4 lg:mb-16 w-full md:w-1/2 animate-in fade-in-5 duration-1000 flex flex-col items-center">
+      <img src="../assets/img/komar.jpg" class="rounded-full w-28 md:w-36 lg:w-32 xl:w-36">
+      <div class="md:flex md:flex-row xl:w-auto xl:mx-52 mt-8 md:mt-8 flex flex-col items-center">
+        <h1 class="text-white text-2xl md:text-4xl lg:text-4xl font-semibold tracking-normal">Привет! Меня зовут </h1>
+        <span class="bg-gradient-to-r from-indigo-400 to-indigo-600 bg-clip-text text-transparent text-4xl lg:text-4xl font-bold mt-2 md:mt-0 md:ml-2 type-mynames"></span>
       </div>
     </div>
-    <div class="lg:grid grid-cols-2 grid-rows-1 lg:gap-4 w-6/12 xl:-mt-48 mx-20 xl:mx-auto -mt-16 pb-12">
-      <AboutBut class="animate-in fade-in-5 duration-1000" />
+    <div class="lg:grid grid-cols-2 grid-rows-1 lg:gap-4 lg:w-6/12 pb-12 lg:flex-row mx-auto w-56">
+      <AboutBut class="animate-in fade-in-5 duration-1000 mb-4 lg:mb-0" />
       <SocBut class="animate-in fade-in-5 duration-1000"/>
     </div>
   </div>
 </template>
 
-<style scoped>
-
+<style>
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+}
 </style>
