9fa3663694737777956aafba1bba2e4c7edf9e01
Pixiemay <kondakov20007.andrey@gmail.com>
2024-06-01 13:12:03 +0700

    Сделал тайтл сайту

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+useHead({
+  title: 'Pixiemays',
+  meta: [
+    { name: 'description', content: 'Pixiemays site.' }
+  ],
+})
+</script>
 <template>
   <div>
     <NuxtPage />
