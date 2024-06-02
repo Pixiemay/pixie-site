2c7dbbe08f72d744504c63a2bf9067af8c52ee79
Pixiemay <kondakov20007.andrey@gmail.com>
2024-06-02 14:01:05 +0700

    Поработал со структурой, сменил чуть чуть социальные сети и добавил github

--- components/main_page/SocBut.vue
+++ components/main_page/SocBut.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLink to="/socials">
-    <div class="bg-zinc-800 hover:bg-zinc-700/60 hover:scale-105 duration-500 ease-in-out w-72 -ml-8 md:w-96 md:mx-auto lg:w-11/12 h-52 lg:h-44 lg:mx-auto rounded-3xl mt-12 md:mt-16 lg:mt-24">
+    <div class="bg-zinc-800 hover:bg-zinc-700/60 hover:scale-105 duration-500 ease-in-out w-72 -ml-8 md:w-96 md:mx-auto lg:w-11/12 h-48 lg:h-44 lg:mx-auto rounded-3xl mt-12 md:mt-16 lg:mt-24">
         <div class="flex flex-row px-8 pt-8 pb-4">
           <icon name="tabler:message-circle" class="size-10 md:size-14 lg:size-10 text-white mr-2"></icon>
           <h1 class="text-2xl md:text-3xl lg:text-2xl text-white font-semibold md:pt-2 lg:leading-5">Социальные сети</h1>
--- components/soc_page/GithubBut.vue
+++ components/soc_page/GithubBut.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <a href="https://github.com/Pixiemay" class="h-52 xl:h-44 bg-zinc-800 hover:bg-zinc-700/60 rounded-3xl px-8 py-6 hover:cursor-pointer hover:scale-105 duration-500 ease-in-out">
+  <a href="https://github.com/Pixiemay" class="h-44 xl:h-44 bg-zinc-800 hover:bg-zinc-700/60 rounded-3xl px-8 py-6 hover:cursor-pointer hover:scale-105 duration-500 ease-in-out">
     <Icon name="mdi:github" class="text-white size-12"/>
     <h1 class="text-white text-2xl font-bold mt-2">GitHub</h1>
     <h2 class="text-gray-400 font-semibold text-base md:text-lg mt-2">Мой github</h2>
